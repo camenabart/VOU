c3901fa7da6e4d83388cd06f745680cb8f02bc2a
Catalina Amenabar <catalina.amenabar.t@gmail.com>
2025-06-16 16:59:11 -0400

    prepare for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/VOU/',
+  base: './',
   server: {
     host: "::",
     port: 8080,
